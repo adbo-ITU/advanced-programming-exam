7adc770e7f690679e8d6d5d1f79c3587b595732d
Adrian Borup <adrian@borup.com>
2024-01-05 10:37:17 +0100

    Add Q2-Q3 tests

--- Exam.test.scala
+++ Exam.test.scala
@@ -5,8 +5,8 @@
 package adpro
 
 import org.scalacheck.{Gen, Arbitrary}
-import org.scalacheck.Arbitrary.arbitrary
-import org.scalacheck.Prop.{forAll, forAllNoShrink}
+import org.scalacheck.Arbitrary.*
+import org.scalacheck.Prop.*
 
 object ExamSpec
   extends org.scalacheck.Properties("exam-2023-autumn"):
@@ -19,6 +19,39 @@ object ExamSpec
     import laziness.LazyList
     forAll { (l: List[Int]) => fViaRec(LazyList(l*)) == fViaFold(LazyList(l*)) }
 
+  property("Q2: longestLine") =
+    import Parsing.{longestLine, allLinesTheSame}
+    import adpro.parsing.Sliceable.*
+
+    forAll { (l: List[List[Int]]) =>
+      (l.nonEmpty && l.forall(_.length > 0)) ==> {
+        val s = l.map(_.mkString(",")).mkString("\n")
+        Right(l.map(_.length).max) == longestLine.run(s)
+      }
+    }
+
+  property("Q3: allLinesTheSame with different length lines") =
+    import Parsing.{longestLine, allLinesTheSame}
+    import adpro.parsing.Sliceable.*
+
+    forAll { (l: List[List[Int]]) =>
+      (l.nonEmpty && l.forall(_.length > 0) && l.map(_.length).toSet.size > 1) ==> {
+        val s = l.map(_.mkString(",")).mkString("\n")
+        allLinesTheSame.run(s) == Right(false)
+      }
+    }
+
+  property("Q3: allLinesTheSame with same length lines") =
+    import Parsing.{longestLine, allLinesTheSame}
+    import adpro.parsing.Sliceable.*
+
+    forAll (Gen.choose(1, 100), Gen.listOfN(100, Gen.choose(1, 100))) {
+      (n: Int, l: List[Int]) =>
+        (l.nonEmpty) ==> {
+          val s = (1 to n).map(_ => l.mkString(",")).mkString("\n")
+          allLinesTheSame.run(s) == Right(true)
+        }
+    }
 end ExamSpec
 
 object NullUpdatesSpecObj
